6d43060c1afee9bb98ef8aa05e179142ac66b1a5
AmazeCode <1558628010@qq.com>
2020-08-08 13:43:53 +0800

    redis项目添加日志操作

--- springboot-redis/src/main/java/com/ac/redis/controller/RedisLockController.java
+++ springboot-redis/src/main/java/com/ac/redis/controller/RedisLockController.java
@@ -1,6 +1,7 @@
 package com.ac.redis.controller;
 
 import com.ac.redis.component.RedisLock;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/redisLock")
+@Slf4j
 public class RedisLockController {
     private final long TIME_OUT = 50 * 1000;
 
@@ -32,18 +34,22 @@ public class RedisLockController {
     @GetMapping("/lock")
     public void lock() {
 
-        // 加锁
-        long currentTime = System.currentTimeMillis();
-        //boolean isLock = redisLock.lock(REDIS_LOCK, String.valueOf(currentTime + TIME_OUT));
-        boolean isLock = redisLock.lockV2(REDIS_LOCK, String.valueOf(currentTime + TIME_OUT),6000l);
-        if (!isLock) {
-            throw new RuntimeException("资源已被抢占，换个姿势再试试吧！");
-        }
+        try{
+            // 加锁
+            long currentTime = System.currentTimeMillis();
+            //boolean isLock = redisLock.lock(REDIS_LOCK, String.valueOf(currentTime + TIME_OUT));
+            boolean isLock = redisLock.lockV2(REDIS_LOCK, String.valueOf(currentTime + TIME_OUT),6000l);
+            if (!isLock) {
+                throw new RuntimeException("资源已被抢占，换个姿势再试试吧！");
+            }
 
-        // do something 若执行其他的过程中，出现异常，则有可能会造成私锁的情况
-        //int i = 1/0;
-        // 解锁
-        redisLock.unLock(REDIS_LOCK);
-        System.out.println("结束了");
+            // do something 若执行其他的过程中，出现异常，则有可能会造成私锁的情况
+            int i = 1/0;
+            // 解锁
+            redisLock.unLock(REDIS_LOCK);
+            log.info("redis lock {}","执行了");
+        }catch (Exception e){
+            log.error("Redis锁错误：{}", e.getMessage(), e);
+        }
     }
 }
